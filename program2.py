b2114e3182d33ccdf7627ce07bf46a7f06e725ed
Andreis Purim <AndreisPurim@gmail.com>
2021-04-19 12:26:46 +0200

    Added PyQt
    Created PyQT basic function with a timer.

--- program2.py
+++ program2.py
@@ -1,3 +1,9 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot, QTimer
+import sys
+import datetime
+
 import wmi
 
 known_processes = {
@@ -20,28 +26,54 @@ delete = {
     
 }
 
-known_processes_list = []
-not_permitted_list = []
-def reemovNestings(l, output):
-    for i in l:
-        if type(i) == list:
-            reemovNestings(i,output)
-        else:
-            output.append(i)
-reemovNestings(known_processes.values(),known_processes_list)
-reemovNestings(not_permitted.values(),not_permitted_list)
+data = {'col1':['1','2','3','4'],
+        'col2':['1','2','1','3'],
+        'col3':['1','1','2','1']}
+ 
+class TableView(QTableWidget):
+    def __init__(self, data, *args):
+        QTableWidget.__init__(self, *args)
+        self.data = data
+        self.setData()
+        self.resizeColumnsToContents()
+        self.resizeRowsToContents()
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.showTime)
+        self.timer.start(60000)
+ 
+    def setData(self): 
+        horHeaders = []
+        for n, key in enumerate(sorted(self.data.keys())):
+            horHeaders.append(key)
+            for m, item in enumerate(self.data[key]):
+                newitem = QTableWidgetItem(item)
+                self.setItem(m, n, newitem)
+        self.setHorizontalHeaderLabels(horHeaders)
 
-# Initializing the wmi constructor
-f = wmi.WMI()
+    def showTime(self):
+        print("Hello this is", datetime.datetime.now())
  
-# Printing the header for the later columns
-print("pid   Process name")
- 
-# Iterating through all the running processes
-for process in f.Win32_Process():
-     
-    # Displaying the P_ID and P_Name of the process
-    
-    if(not process.Name in known_processes_list and not process.Name in not_permitted_list):
-       print(f"{process.ProcessId:<10} {process.Name}")
-    
+def main(args):
+    app = QApplication(args)
+    table = TableView(data, 4, 3)
+    table.show()
+    sys.exit(app.exec_())
+
+if __name__=="__main__":
+    known_processes_list = []
+    not_permitted_list = []
+    def reemovNestings(l, output):
+        for i in l:
+            if type(i) == list:
+                reemovNestings(i,output)
+            else:
+                output.append(i)
+    reemovNestings(known_processes.values(),known_processes_list)
+    reemovNestings(not_permitted.values(),not_permitted_list)
+    f = wmi.WMI()
+    print("pid   Process name")
+    for process in f.Win32_Process():
+        if(not process.Name in known_processes_list and not process.Name in not_permitted_list):
+            print(f"{process.ProcessId:<10} {process.Name}")
+    main(sys.argv)
+
